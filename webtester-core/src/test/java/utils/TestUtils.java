66d8f9d1d75664616ec6ab735439de3953078a07
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2016-06-18 23:53:38 +0200

    Added WebDriverBrowser tests

--- webtester-core/src/test/java/utils/TestUtils.java
+++ webtester-core/src/test/java/utils/TestUtils.java
@@ -1,5 +1,6 @@
 package utils;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.UndeclaredThrowableException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -12,6 +13,26 @@ public final class TestUtils {
             return new URL(url);
         } catch (MalformedURLException e) {
             throw new UndeclaredThrowableException(e);
+        }
+    }
+
+    /**
+     * Sets the value for a field with the given name of the given object using reflection.
+     * <p>
+     * <b>Note:</b> the field's visibility will be changed in order for this operation to work.
+     *
+     * @param object the object who's field value should be set
+     * @param fieldName the name of the field
+     * @param value the value to set
+     * @throws IllegalArgumentException in case of an error
+     */
+    public static void setFieldValue(Object object, String fieldName, Object value) {
+        try {
+            Field field = object.getClass().getDeclaredField(fieldName);
+            field.setAccessible(true);
+            field.set(object, value);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new IllegalArgumentException("could not set field value of '" + fieldName + "'", e);
         }
     }
 
