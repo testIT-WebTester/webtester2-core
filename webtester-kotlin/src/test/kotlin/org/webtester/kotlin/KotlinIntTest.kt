13afa8e91adaea35ebd17e99eab04a9652a89bbd
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2017-06-02 14:50:39 +0200

    added Kotlin module with tests
    Currently this will just provide Kotlin tests, but in the future there might be production classes for a more Kotlin-esk API.

---
+++ webtester-kotlin/src/test/kotlin/org/webtester/kotlin/KotlinIntTest.kt
@@ -0,0 +1,43 @@
+package org.webtester.kotlin
+
+import info.novatec.testit.webtester.pagefragments.TextField
+import info.novatec.testit.webtester.pagefragments.annotations.IdentifyUsing
+import info.novatec.testit.webtester.pages.Page
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import utils.integration.BaseIntTest
+import javax.annotation.PostConstruct
+
+class KotlinIntTest : BaseIntTest() {
+
+    override fun getHTMLFilePath(): String = "html/testKotlin.html"
+
+    @Test
+    fun `basic WebTester functions are working`() {
+        val page = create(TestKotlinPage::class.java)
+        assertThat(page.getValue()).isEqualTo("did something")
+        page.setValue("Foo Bar?")
+        assertThat(page.getValue()).isEqualTo("Foo Bar?")
+    }
+
+    interface TestKotlinPage : Page {
+
+        @IdentifyUsing("#emptyTextField")
+        fun textField(): TextField
+
+        @PostConstruct
+        fun doSomething() {
+            textField().text = "did something"
+        }
+
+        fun setValue(value: String) {
+            textField().text = value
+        }
+
+        fun getValue(): String {
+            return textField().text
+        }
+
+    }
+
+}
